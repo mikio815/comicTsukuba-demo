2008c05bdabfa63e4dc09b9c48edba111cf02012
mikio815 <uaru0815@gmail.com>
2025-01-30 21:59:48 +0900

    Headerをかっこよくした

--- src/app/(common)/header.tsx
+++ src/app/(common)/header.tsx
@@ -2,18 +2,28 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="bg-blue-600 text-white py-4 px-6 shadow-md">
+        <header className="bg-black text-white py-4 px-6 shadow-md font-custom">
             <div className="container mx-auto flex justify-between items-center">
                 <Link href="/" className="text-xl font-bold">コミックつくば！</Link>
                 <nav>
-                    <ul className="flex space-x-4">
-                        <li><Link href="/about">コミつく！とは？</Link></li>
-                        <li><Link href="/info">開催概要</Link></li>
-                        <li><Link href="/application">参加申込</Link></li>
-                        <li><Link href="contact">Contact</Link></li>
+                    <ul className="flex space-x-1 text-xs">
+                        <li className=" px-3 py-1 group hover:bg-white hover:text-black">
+                            <Link href="/about">コミつく！とは？</Link>
+                        </li>
+                        <li className="px-3 py-1 group hover:bg-white hover:text-black">
+                            <Link href="/info">開催概要</Link>
+                        </li>
+                        <li className=" px-3 py-1 group hover:bg-white hover:text-black">
+                            <Link href="/application">参加申込</Link>
+                        </li>
+                        <li className=" px-3 py-1 group hover:bg-white hover:text-black">
+                            <Link href="/contact">Contact</Link>
+                        </li>
                     </ul>
                 </nav>
             </div>    
         </header>
     );
 }
+
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,16 @@ const config: Config = {
         primary: "#2563EB", // 青系のカスタムカラー
         secondary: "#9333EA", // 紫系のカスタムカラー
       },
+      borderWidth: {
+        0.5: "0.5px",
+        0.3: "0.3px"
+      },
+      fontFamily: {
+        custom: ['DelaSukoGothic', 'sans-serif'],
+      },
+      boxShadow: {
+        'text-outline': '0 0 5px rgba(0, 0, 0, 0.7)'
+      },
     },
   },
   plugins: [],
