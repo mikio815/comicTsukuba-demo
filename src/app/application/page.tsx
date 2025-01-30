dc65932015c8b5b1e1e3eba50c78f7d54d6a1980
mikio815 <uaru0815@gmail.com>
2025-01-30 16:32:52 +0900

    また整備

---
+++ src/app/application/page.tsx
@@ -0,0 +1,11 @@
+import Layout from "../(common)/layout";
+
+export default function ApplyPage() {
+    return (
+        <Layout>
+            <div className="container mx-auto py-8 text-center">
+                <h1 className="text-3xl font-bold">工事中です！</h1>
+            </div>
+        </Layout>
+    );
+}
