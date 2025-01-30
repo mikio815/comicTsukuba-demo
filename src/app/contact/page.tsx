b25708b3b16fbde66d80cda6a7409b85a446fe43
mikio815 <uaru0815@gmail.com>
2025-01-30 16:44:00 +0900

    工事中、contactを編集

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
 import Layout from "../(common)/layout";
 
 export default function ContactPage() {
     return (
         <Layout>
             <div className="container mx-auto py-8 text-center">
-                <h1 className="text-3xl font-bold">工事中です！</h1>
+                <h1 className="text-3xl font-bold">Contact</h1>
+                <p className="mt-4">
+                    <strong>Twitter(現X):</strong> <Link href="https://x.com/comictsukuba" className="text-blue-500 hover:text-blue-700 underline">@comictsukuba</Link>
+                </p>
             </div>
         </Layout>
     );
