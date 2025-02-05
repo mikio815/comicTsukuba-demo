f6ab6717144e0a8dced40fbad859a2e51d673f34
mikio815 <uaru0815@gmail.com>
2025-02-05 15:08:24 +0900

    add app-key

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,7 +1,8 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url:env('PUBLIC_URL', 'https://comictsukuba-01.onrender.com'),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['mX8v9h2KpQyR', '4b7FsZdLwN1T']),
   },
 });
