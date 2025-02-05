107bc1f5383d08c72187f4de9223f8eead5904e1
mikio815 <uaru0815@gmail.com>
2025-02-05 19:54:08 +0900

    Promise

--- frontend/src/app/news/[id]/page.tsx
+++ frontend/src/app/news/[id]/page.tsx
@@ -7,9 +7,6 @@ type NewsDetailProps = {
   params: Promise<{
     id: string;
   }>;
-  searchParams?: {
-    [key: string]: string | string[] | undefined;
-  };
 };
 
 export default async function NewsDetailPage({ params }: NewsDetailProps) {
