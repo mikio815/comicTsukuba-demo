bf3c8f8397acb4977247d9e098abda48007fd4bf
mikio <uaru0815@gmail.com>
2025-03-19 04:17:04 +0900

    Delete backend directory

--- backend/config/server.ts
+++
@@ -1,8 +0,0 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url:env('PUBLIC_URL', 'https://comictsukuba-01.onrender.com'),
-  app: {
-    keys: env.array('APP_KEYS', ['mX8v9h2KpQyR', '4b7FsZdLwN1T']),
-  },
-});
