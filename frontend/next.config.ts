89c9ca4a13a366c17a911570a328a0ba723cdf37
mikio <uaru0815@gmail.com>
2025-03-31 18:23:38 +0900

    リダイレクトの解決

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,14 +4,14 @@ import type { Redirect } from "next/dist/lib/load-custom-routes";
 const redirects = async (): Promise<Redirect[]> => {
   return [
     {
-      source: "/",
+      source: "/:path*",
       has: [
         {
           type: "host",
           value: "comic-tsukuba.com",
         },
       ],
-      destination: "https://www.comic-tsukuba.com",
+      destination: "https://www.comic-tsukuba.com/:path*",
       permanent: true,
     },
   ];
