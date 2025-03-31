72afdb7d414e3a339f8a42dfa1a544418c410b05
mikio <uaru0815@gmail.com>
2025-03-31 17:51:54 +0900

    リダイレクトの解決

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
+import type { Redirect } from "next/dist/lib/load-custom-routes";
+
+const redirects = async (): Promise<Redirect[]> => {
+  return [
+    {
+      source: "/",
+      has: [
+        {
+          type: "host",
+          value: "comic-tsukuba.com",
+        },
+      ],
+      destination: "https://www.comic-tsukuba.com",
+      permanent: true,
+    },
+  ];
+};
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  redirects,
 };
 
 export default nextConfig;
