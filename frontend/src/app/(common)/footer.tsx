1dcd65fd38408f090a40b9468306e19e87df66c1
mikio <uaru0815@gmail.com>
2025-04-02 17:04:24 +0900

    レイアウト修正

--- frontend/src/app/(common)/footer.tsx
+++ frontend/src/app/(common)/footer.tsx
@@ -3,7 +3,7 @@ import LinkButton from "@/app/(common)/linkButton"
 
 export default function Footer() {
     return (
-        <footer className="bg-[#00C1D4] font-nav text-white text-xs h-[120px] flex items-center justify-between shadow-md pl-40 pr-60 space-y-5">
+        <footer className="bg-[#00C1D4] font-nav text-white text-xs py-6 md:px-40 flex items-center justify-between shadow-md pl-10 md:pl-40 space-y-5">
             <div className="space-y-5">
                 <div>
                     <p className="text-[12px] font-bold">
@@ -16,7 +16,7 @@ export default function Footer() {
                 </div>
             </div>
 
-            <div className="mt-20 flex pl-28 space-x-10">
+            <div className="mt-20 flex space-x-10 md:mr-30 mr-10 ml-20">
                 <LinkButton platform="twitter" url="https://x.com/comictsukuba" size={36} color="#FFFFFF"/>
                 <LinkButton platform="instagram" url="https://instagram.com" size={36} color="#FFFFFF" />
             </div>
